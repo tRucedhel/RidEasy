995ebd2c8f94a807d81a26bfe1fb2bca7857dfe8
reindri <regitaindri02@gmail.com>
2022-11-17 14:38:44 +0700

    nyoba branch

---
+++ app/src/main/java/com/project/driveasy/Signup_driver.java
@@ -0,0 +1,14 @@
+package com.project.driveasy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup_driver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_driver);
+    }
+}
